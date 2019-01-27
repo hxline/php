2266c4aca9905232bbaf1da19612ff646b318305
Handoyo <handoyo@mitrais.com>
2019-01-27 13:40:54 +0700

    tes

--- web/index.php
+++ web/index.php
@@ -35,7 +35,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
-<p>Tesd</p>
+<p>TesdOK</p>
     <table border="1">
         <tr>
             <th width="50px">DATA</th>
